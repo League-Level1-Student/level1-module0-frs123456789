ae910953f625434899897ee4933e3caa791a7f87
munchman <nootnoot@gmail.com>
2018-10-17 19:54:52 -0700

    Photo Quiz and My Drum Kit

--- src/PhotoQuiz.java
+++ src/PhotoQuiz.java
@@ -22,35 +22,46 @@ public class PhotoQuiz {
                 quizWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   // This will make sure the program exits when you close the window
 
 		// 1. find an image on the internet, and put its URL in a String variable (from your browser, right click on the image, and select “Copy Image Address”)
-
+String FunnyPic = "https://imgix.ranker.com/user_node_img/50065/1001280631/original/dr-no-chill-photo-u1?w=650&q=50&fm=jpg&fit=crop&crop=faces";
 		// 2. create a variable of type "Component" that will hold your image
-
+Component image;
 		// 3. use the "createImage()" method below to initialize your Component
-
+image = createImage(FunnyPic);
 		// 4. add the image to the quiz window
-
+quizWindow.add(image);
 		// 5. call the pack() method on the quiz window
-
+quizWindow.pack();
 		// 6. ask a question that relates to the image
-
+String answer1 = JOptionPane.showInputDialog("Do y'all like some spicy memes? hehe");
 		// 7. print "CORRECT" if the user gave the right answer
-
+if(answer1 .equals("yes") ) {
+	JOptionPane.showMessageDialog(null, "CORRECT");
+}
 		// 8. print "INCORRECT" if the answer is wrong
-
+else if (answer1 .equals("no") ) {
+	JOptionPane.showMessageDialog(null, "INCORRECT");
+}
 		// 9. remove the component from the quiz window (you may not see the effect of this until step 12)
-
+quizWindow.remove(image);
 		// 10. find another image and create it (might take more than one line of code)
-
+String funnypic2 = ("https://i.chzbgr.com/full/9097238528/h53420778/");
+Component image2;
+image2= createImage(funnypic2);
 		// 11. add the second image to the quiz window
-
+quizWindow.add(image2);
 		// 12. pack the quiz window
-
+quizWindow.pack();
 		// 13. ask another question
-
+String answer2 = JOptionPane.showInputDialog("Do you like HALFros?!? hehehe");
 		// 14+ check answer, say if correct or incorrect, etc.
-
+if(answer2 == "yes") {
+	JOptionPane.showMessageDialog(null,"YAAAAAAS, RIGHT ANSWER.");
+}
+	
+	else{
+		JOptionPane.showMessageDialog(null, "WRONG!");
 	}
-
+	}
 	private static Component createImage(String imageUrl) throws MalformedURLException {
 		URL url = new URL(imageUrl);
 		Icon icon = new ImageIcon(url);
